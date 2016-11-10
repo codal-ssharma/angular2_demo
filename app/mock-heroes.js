6eb04e833a3d9017b3ff18272032a38d3a98a733
Shivani Sharma <ssharma@codal.com>
2016-11-10 20:59:31 +0530

    Angular quickstart heroes app created

---
+++ app/mock-heroes.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.HEROES = [
+    { id: 1, name: 'Doctor Strange' },
+    { id: 2, name: 'The Ancient One' },
+    { id: 3, name: 'Bombasto' },
+    { id: 4, name: 'Celeritas' },
+    { id: 5, name: 'Magneta' },
+];
+exports.HERO = {
+    id: 1,
+    name: 'Mr. Nice'
+};
+//# sourceMappingURL=mock-heroes.js.map
---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Doctor Strange' },
+  { id: 2, name: 'The Ancient One' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+];
+
+export const HERO: Hero = {
+  id: 1,
+  name: 'Mr. Nice'
+};
