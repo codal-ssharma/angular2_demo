d6b4fc24a2cde53bebaf7cb8123c3a6a344c3a51
Shivani Sharma <ssharma@codal.com>
2016-11-11 17:46:33 +0530

    Routing and dashboard component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }   from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
 import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
   ],
-  providers: [ HeroService ],
+  providers: [
+    HeroService
+  ],
   bootstrap: [ AppComponent ]
 })
 
